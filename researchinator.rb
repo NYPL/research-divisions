8164a9e5797aa89a98030ed03b5b9f463b5a90c9
Edwin Guzman <edwin.gzmn@gmail.com>
2015-11-13 14:05:42 -0500

    Using the ENV variable or the default.

--- researchinator.rb
+++ researchinator.rb
@@ -6,7 +6,20 @@ require 'erb'
 
 class Researchinator < Sinatra::Base
   configure do
-    set :researchinator_env, ENV['RESEARCHINATOR_ENV']
+    configs = JSON.parse(File.read('researchinator.json'))
+    if configs["environments"].has_key?(ENV['RESEARCHINATOR_ENV'])
+      set :env_config, configs["environments"][ENV['RESEARCHINATOR_ENV']]
+    else
+      set :env_config, 
+        configs["environments"][configs["environments"]["default"]]
+    end
+
+    if ENV['RESEARCHINATOR_ENV'].nil?
+      set :researchinator_env, configs["environments"]["default"]
+    else
+      set :researchinator_env, ENV['RESEARCHINATOR_ENV']
+    end
+
     if settings.researchinator_env === 'development'
       set :refinery_api, 'dev-'
     elsif settings.researchinator_env === 'qa'
@@ -15,13 +28,6 @@ class Researchinator < Sinatra::Base
       set :refinery_api, ''
     end
 
-    configs = JSON.parse(File.read('researchinator.json'))
-    if configs["environments"].has_key?(ENV['RESEARCHINATOR_ENV'])
-      set :env_config, configs["environments"][ENV['RESEARCHINATOR_ENV']]
-    else
-      set :env_config, 
-        configs["environments"][configs["environments"]["default"]]
-    end
     set :divisions_with_appointments, configs["divisions_with_appointments"]
     set :featured_amenities, configs["featured_amenities"]
     set :research_order, configs["research_order"]
